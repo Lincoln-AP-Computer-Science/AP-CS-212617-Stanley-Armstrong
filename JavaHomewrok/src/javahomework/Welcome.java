27fe30d551dbf8ecc6e9af10067e94fbf860d66a
stonewallstan <stanleyarmstrong31@gmail.com>
2016-08-21 01:20:05 -0700

    Welcome Class

---
+++ JavaHomewrok/src/javahomework/Welcome.java
@@ -0,0 +1,10 @@
+package javahomework;
+public class Welcome{
+    public static void main(String[] args){
+        System.out.println("Pikachu welcomes you to the world of Pokemon!");
+        System.out.println("(\\__/)");
+        System.out.println("(o^.^)");
+        System.out.println("z(_(\")(\")");
+    }
+    
+}
