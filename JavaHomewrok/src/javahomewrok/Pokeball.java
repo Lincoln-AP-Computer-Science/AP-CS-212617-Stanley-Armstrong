25cc2c6119ec7bd4f1ab4b95dacd271b780f6456
stonewallstan <stanleyarmstrong31@gmail.com>
2016-08-23 20:16:19 -0700

    Added the Worksheet for homework for 8/23/16
    The worksheet is finished and the output is correct.

---
+++ JavaHomewrok/src/javahomewrok/Pokeball.java
@@ -0,0 +1,40 @@
+package javahomewrok;
+
+public class Pokeball{
+    public static void main(String[] args){
+        //#1 Starts Here
+        System.out.println("   @@@@@@@@");
+        System.out.println("  @@@@@@@@@@@");
+        System.out.println(" @@@@@@@@@@@@@");
+        System.out.println("@@@@@@@@@@@@@@@");
+        System.out.println("||||[\"\"\"\"]||||");
+        System.out.println("||||[,,,,]||||");
+        System.out.println("OOOOOOOOOOOOOOO");
+        System.out.println(" OOOOOOOOOOOOO");
+        System.out.println("   OOOOOOOOOO");
+        System.out.println("     OOOOOOO");
+        //#2 Starts here
+        //This pokeball graphic can be used in the opening sequence of my Pokemon game, or any time the player needs to catch a wild Pokemon.
+        //#3 Starts Here
+        /*
+        If you want to
+        leave a note for future programmers
+        in your code
+        place two slashes, like this //
+        in front of each line of your comment
+        Another  alternative is to enclose your comments with
+        /* but, but not\*!
+        */
+        //#4 Starts Here
+        // Organization because the file name of a java file always has to match the class name.
+        //#5 Starts Here
+        //Errors: forgot class, main is mispelled, forgot brace,println is capitalized with a P, forgot a semi-colon, forgot to close a string literal
+        //#6 Starts Here
+        /*
+        I looked for comon mistakes and spelling errors. 
+       
+        */
+    }
+}
+
+ 
