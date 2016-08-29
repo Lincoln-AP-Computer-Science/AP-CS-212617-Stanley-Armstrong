12726b101cf722fe207d43c13ecc0db47d9259ee
stonewallstan <stanleyarmstrong31@gmail.com>
2016-08-28 23:20:15 -0700

    Homework That I finished

---
+++ JavaHomewrok/src/javahomewrok/HouseOfJacks.java
@@ -0,0 +1,10 @@
+package javahomewrok;
+
+public class HouseOfJacks{
+    public static void building(){
+        System.out.println("This is the house that Jack built.\n\nThis is the malt\nThat lay in the house that Jack built.\n\nThis is the rat,\nThat ate the malt\nThat lay in thehouse that Jack built\n\nThis is the cat,\nThat killed the rat,\nThat ate the malt\nThat lay in the house that Jack built.\n\nThis is the dog,\nThat worried the cat,\nThat killed the rat,\nThat ate the malt\nThat lay in the house that Jack built.\n\nThis is the cow with the crumpled horn,\nThat tossed the dog\nThat worriedthe cat,\nThat killed the rat\nThat ate the malt,\nThat lay in the house that Jack built.\n\nThis is the maiden all forlorn\nThat milked the cow with the crumpled horn,\nThat tossed the dog\nThat worried the cat,\nThat killed the rat,\nThat ate the malt\nThat lay in the house that Jack built.");
+    }
+    public static void main(String[] args){
+        building();
+    }
+}
---
+++ JavaHomewrok/src/javahomewrok/Number3.java
@@ -0,0 +1,21 @@
+package javahomewrok;
+
+public class Number3{
+    public static void oldWoman(){
+        System.out.println("There was an old lady who swallowed a fly");
+        System.out.println("I don't know why she swallowed the fly");
+        System.out.println("Perhaps she'll die");
+        System.out.println("But it's only a fly");
+        System.out.println("I think I'll cry");
+        System.out.println("She gulped it out of the sky");
+        System.out.println("Oh my!");
+        System.out.println();
+        System.out.println("There was an old lady who swallowed a spider \nThat wiggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly/nI don't know why\n She swallowed the fly\n Perhaps she'll die\nGone to the by and by\nSigh");
+        System.out.println("There was an old lady who swallowed a bird\nHow absurd! She swallowed a bird!\nShe swallowed the bird to catch the spider.\nShe swallowed the bird to catch the spider.\nShe swallowed the spider to catch the fly.\nI don't know why \nShe swallowed the fly\nPerhaps she'll die.\nShe'll leave us high and dry.");
+        System.out.println();
+        System.out.println("There was an old lady who swallowed a cat.\nImagine that! She swallowed a cat.\nShe swallowed the cat to catch the bird.\nShe swallowed the bird to catch the spider.\nShe swallowed the spider to catch the fly.\nI don't know why \n She swallowed the fly\n Perhaps she'll die.\n I hope it's a lie.\n\nThere was an old lady who swallowed a dog.\nShe went whole hog to swallow the dog.\nShe swallowed the dog to catch the cat.\nShe swallowed the cat to catch the bird.\nShe swallowed the bird to catch the spider.\nShe swallowed the spider to catch the fly.\nI don't why \n She swallowed the fly\n Perhaps she'll die.\nThere's a tear in my eye.\n\n There was an old lady who swallowed a cow.\n I don't know how she swallowed the cow.\n She swallowed the cow to catch the dog.\nShe swallowed the dog to catch the cat.\nShe swallowe the cat to catch the bird.\nShe swallowed the bird to catch the spider.\nShe swallowed the bird to catch the fly.\nI don't know why\nShe swallowed the fly\nPerhaps she'll die.\nI'd rather have ham on rye.\nAnd she had a frog on the sly.\nShe did it in one try.\n\nThere was an old lady who swallowed a horse.\nShe died, of course.\nIt was the last course.\nI'm filled with remorse.\nWhat's left to say...\nEven the artist is crying.\nWe'll miss her dearly.\nIt is such a loss.\n She had no time to floss.\nShe missed out on the sauce.\n\nMoral: Never swallow a horse.");
+    }
+    public static void main(String[] args){
+        oldWoman();
+    }
+}
