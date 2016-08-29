991a024d0fe0d653ec9a7edcf40d13185aa3e1f8
Stanley <stanleyarmstrong31@gmail.com>
2016-08-29 05:27:04 +0000

    Need my homework that I worked on in class

---
+++ JavaHomewrok/MISSISSPPI.java
@@ -0,0 +1,17 @@
+public class MISSISSPPI{
+   public static void m(){
+   System.out.println("M     M    IIIII     SSSSS      PPPPPP");
+   System.out.println("MM   MM      I      S     S     P     P");
+   System.out.println("M M M M      I      S           P     P");
+   System.out.println("M  M  M      I       SSSSS      PPPPPP");
+   System.out.println("M     M      I            S     P");
+   System.out.println("M     M      I      S     S     P");
+   System.out.println("M     M    IIIII     SSSSS      P");
+   }
+   
+   public static void main(String[] args){
+   m();
+   
+   }
+   }
+   
---
+++ JavaHomewrok/OldLady.java
@@ -0,0 +1,11 @@
+public class OldLady{
+   OldLady(){
+      String[] words = {"fly" ,"spider" , "bird" , "cat" , "dog" , "cow" , "horse" }; ;
+      for(int i = 0 ; i <= words.length , i++){
+      switch(i){
+      case
+         System.out.println("There was an old lady who swallowed a " + words[i] );
+         System.out.println("I don't know why she swallowed the " + words[i]);
+         System.out.println("Perhaps she'll die");
+         System.out.println
+         
