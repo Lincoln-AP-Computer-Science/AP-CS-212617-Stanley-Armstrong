540e51a360342d48d2ed8cb1d1a0655a6db41961
Stanley Armstrong <stanleyarmstrong31@gmail.com>
2016-08-29 08:54:36 -0700

    Sync

---
+++ JavaHomewrok/src/javahomewrok/DrawRectangle.java
@@ -0,0 +1,18 @@
+package javahomewrok;
+
+public class DrawRectangle{
+    public static void drawHorizontal(){
+        System.out.println("+---+");
+    }
+    public static void drawVertical(){
+        System.out.println("|   |");
+    }
+    public static void main(String[] args){
+        drawHorizontal();
+        drawVertical();
+        drawVertical();
+        drawVertical();
+        drawHorizontal();
+        
+    }
+}
--- JavaHomewrok/src/javahomewrok/JavaHomewrok.java
+++ JavaHomewrok/src/javahomewrok/JavaHomewrok.java
@@ -15,7 +15,9 @@ public class JavaHomewrok {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        DrawRectangle j = new DrawRectangle();
         
-    }
     
+    
+}
 }
